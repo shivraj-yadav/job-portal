3d68667015769445a554efc531b3f14d9312115e
shivraj-yadav <shivrajyadav320@gmail.com>
2026-02-10 01:28:25 +0530

    chore: setup MongoDB connection and create initial Mongoose models (User, Job, Company, Application)

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
@@ -21,5 +23,6 @@ app.get('/home', (req, res) => {
 });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
